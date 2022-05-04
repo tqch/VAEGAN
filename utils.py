ce0847203f78b42411f6c90bd98f009c7686247f
tqch <tqch2020@gmail.com>
2022-05-04 02:27:21 -0500

    apply recursive call of str2dict

--- utils.py
+++ utils.py
@@ -4,6 +4,10 @@ def dict2str(d):
         out_str.append(str(k))
         if isinstance(v, (list, tuple)):
             v = "_".join(list(map(str, v)))
+        elif isinstance(v, float):
+            v = f"{v:.0e}"
+        elif isinstance(v, dict):
+            v = dict2str(v)
         out_str.append(str(v))
     out_str = "_".join(out_str)
     return out_str
